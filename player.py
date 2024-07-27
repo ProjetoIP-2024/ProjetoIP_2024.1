5700969d4a766d294b6d99794117fc9bb332fd2c
ProjetoIP-2024 <projetoip2024@gmail.com>
2024-07-27 14:18:15 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,14 +1,6 @@
 import pygame
 from laser import Laser
 
-pygame.init()
-
-largura_tela = 1920
-altura_tela = 1080
-tela = pygame.display.set_mode((largura_tela, altura_tela))
-
-import pygame
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, tela, x, y, largura_tela):
         super().__init__()
@@ -20,18 +12,31 @@ class Player(pygame.sprite.Sprite):
         self.y = y
         self.largura_tela = largura_tela
         self.velocidade = 5
-        self.shoot = False  # Novo atributo para indicar se o jogador está disparando
+        self.shoot = False
+        self.vida = 5
 
     def control(self):
         teclas = pygame.key.get_pressed()
         if teclas[pygame.K_LEFT]:
-            if self.rect.left > 0:  # Verifica se o jogador não ultrapassa a borda esquerda da tela
+            if self.rect.left > 0:
                 self.rect.x -= self.velocidade
         if teclas[pygame.K_RIGHT]:
-            if self.rect.right < self.largura_tela:  # Verifica se o jogador não ultrapassa a borda direita da tela
+            if self.rect.right < self.largura_tela:
                 self.rect.x += self.velocidade
         if teclas[pygame.K_SPACE]:
-            self.shoot = True  # Define o flag de disparo como True
+            self.shoot = True
 
     def draw(self):
         self.tela.blit(self.sprite, self.rect)
+        self.desenhar_vida()
+
+    def desenhar_vida(self):
+        fonte = pygame.font.SysFont(None, 36)
+        vida_texto = fonte.render(f'Vida: {self.vida}', True, (255, 0, 0))
+        self.tela.blit(vida_texto, (10, 10))
+
+    def receber_dano(self, dano):
+        self.vida -= dano
+        if self.vida < 0:
+            return False
+        return True
