c8dd8f1f9b06ae7e7ceb431f9d9ba3bfef198fc5
ProjetoIP-2024 <projetoip2024@gmail.com>
2024-07-28 06:48:49 -0300

    Update coletavel.py

--- coletavel.py
+++ coletavel.py
@@ -3,10 +3,10 @@ import pygame
 class Coletavel(pygame.sprite.Sprite):
     def __init__(self, x, y, tipo):
         super().__init__()
-        self.image = pygame.Surface((20, 20))  # Tamanho do coletável
-        self.image.fill((0, 255, 0))  # Cor verde como exemplo; substitua com a imagem real
-        self.rect = self.image.get_rect(topleft=(x, y))
         self.tipo = tipo  # Tipo de coletável: 'moeda', 'sucata', etc.
+        self.sprite = pygame.image.load('./imagens/' + self.tipo + '.png')
+        self.sprite = pygame.transform.scale(self.sprite, (50, 50))
+        self.rect = self.sprite.get_rect(center=(x, y))
 
     def update(self):
         self.rect.y += 5  # Velocidade de descida; ajuste conforme necessário
