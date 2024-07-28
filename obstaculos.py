8a817d3767660178d277ff3a9432feb5e1af95c6
ProjetoIP-2024 <projetoip2024@gmail.com>
2024-07-28 06:50:01 -0300

    Update obstaculos.py

--- obstaculos.py
+++ obstaculos.py
@@ -1,16 +1,15 @@
-import pygame as pg
+import pygame
 
 class Objeto:
-    def __init__(self, win, x, y, cor):
-        self.win = win
-        self.largura = 130
-        self.altura = 20
-        self.vel = 5 
-        self.rect = pg.Rect(x, y, self.largura, self.altura)
-        self.cor = cor
+    def __init__(self, x, y, tela):
+        self.sprite = pygame.image.load('./imagens/tronco.jpeg')
+        self.sprite = pygame.transform.scale(self.sprite, (130, 20))
+        self.rect = self.sprite.get_rect(topleft=(x, y))
+        self.vel = 5
+        self.tela = tela
 
     def movimentacao(self):
         self.rect.x += self.vel
 
     def draw(self):
-        pg.draw.rect(self.win, self.cor, (self.rect.x, self.rect.y, self.largura, self.altura))
+        self.tela.blit(self.sprite, self.rect)
