1568af87797da770ec4fdf5e92279aa992555fce
ProjetoIP-2024 <projetoip2024@gmail.com>
2024-07-29 09:45:54 -0300

    laser.py

--- laser.py
+++ laser.py
@@ -1,17 +1,21 @@
 import pygame
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, pos, speed, imagem):
+    def __init__(self, pos, speed, angulo, imagem):
         super().__init__()
         self.sprite = pygame.image.load('./imagens/' + imagem +'.png')
         self.sprite = pygame.transform.scale(self.sprite, (50, 50))
         self.rect = self.sprite.get_rect(center=pos)
         self.speed = speed
+        self.angulo = angulo
 
     def update(self):
         self.rect.y += self.speed
+        self.rect.x += self.angulo
         self.destruicao()
 
     def destruicao(self):
         if self.rect.y < 0:
             self.kill()
+        
+
